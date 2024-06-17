83b13c445431857fd4bb223057ec466069c91c6b
Nadeem <nadeemnagarji@gmail.com>
2024-06-17 16:30:35 +0530

    done the setup of  the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button.tsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
